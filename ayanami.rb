55f9910562610299acb40804cc2358e840280066
Johannes Siipola <johannes@siipo.la>
2016-11-12 00:38:15 +0200

    Get URL and parse title tag

--- ayanami.rb
+++ ayanami.rb
@@ -1,4 +1,6 @@
 require 'cinch'
+require 'net/http'
+require 'nokogiri'
 
 bot = Cinch::Bot.new do
   configure do |c|
@@ -7,10 +9,26 @@ bot = Cinch::Bot.new do
     c.nick = "ayanami"
   end
 
+  helpers do
+    def getTitle(url)
+      uri = URI(url)
+      html = Net::HTTP.get(uri)
+      dom = Nokogiri(html)
+      title = dom.css('title').first
+      if title
+        return title.content
+      end
+    end
+  end
+
   on :message, /(https?:\/\/\S*)/ do |m, url|
-    m.reply "Hello, #{m.user.nick}"
-    debug "message contains url"
-    debug url
+    # m.reply "Hello, #{m.user.nick}"
+    # debug "message contains url"
+    title = getTitle(url)
+    if title
+      m.reply title
+    end
+    # debug url
   end
 end
 
